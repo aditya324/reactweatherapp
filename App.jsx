351ee801ba264b3f95bb6b581774ee63f7a80e90
aditya324 <64701007+aditya324@users.noreply.github.com>
2023-08-28 12:04:24 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import WeatherApp from './Components/Weatherapp/WeatherApp'
+
+const App = () => {
+  return (
+    <div className='App'>
+      <WeatherApp/>
+    </div>
+  )
+}
+
+export default App
